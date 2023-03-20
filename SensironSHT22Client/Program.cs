4c83cb6e6fde76a2939117e8fad531ed7d996405
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-03-20 18:02:10 +1300

    Initial version of nanoFramework SHT20 library test client

--- SensironSHT22Client/Program.cs
+++ SensironSHT22Client/Program.cs
@@ -1,20 +1,64 @@
-using System;
-using System.Diagnostics;
-using System.Threading;
-
-namespace SensironSHT22Client
+//---------------------------------------------------------------------------------
+// Copyright (c) March 2023, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// nanoff --platform ESP32 --serialport COM7 --update
+// nanoff --target ST_STM32F769I_DISCOVERY --update 
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.nanoFramework.Sensirion
 {
-    public class Program
+    using System;
+    using System.Device.I2c;
+    using System.Threading;
+
+    //using nanoFramework.Hardware.Esp32;
+
+    class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            Debug.WriteLine("Hello from nanoFramework!");
+            const int busId = 1;
 
-            Thread.Sleep(Timeout.Infinite);
+            //Configuration.SetPinFunction(Gpio.IO04, DeviceFunction.I2C1_DATA);
+            //Configuration.SetPinFunction(Gpio.IO05, DeviceFunction.I2C1_CLOCK);
 
-            // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/
-            // Join our lively Discord community: https://discord.gg/gCyBu8T
+            I2cConnectionSettings i2cConnectionSettings = new(busId, Sht20.DefaultI2cAddress);
+
+            using I2cDevice i2cDevice = I2cDevice.Create(i2cConnectionSettings);
+            {
+                using (Sht20 sht20 = new Sht20(i2cDevice))
+                {
+                    sht20.Reset();
+
+                    while (true)
+                    {
+                        double temperature = sht20.Temperature();
+                        double humidity = sht20.Humidity();
+#if HEATER_ON_OFF
+					    sht20.HeaterOn();
+					    Console.WriteLine($"{DateTime.Now:HH:mm:ss} HeaterOn:{sht20.IsHeaterOn()}");
+#endif
+                        Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Temperature:{temperature:F1}°C Humidity:{humidity:F0}% HeaterOn:{sht20.IsHeaterOn()}");
+#if HEATER_ON_OFF
+					    sht20.HeaterOff();
+					    Console.WriteLine($"{DateTime.Now:HH:mm:ss} HeaterOn:{sht20.IsHeaterOn()}");
+#endif
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
         }
     }
 }
