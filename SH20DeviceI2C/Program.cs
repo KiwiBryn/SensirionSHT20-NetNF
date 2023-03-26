0c66752b06fb55bc24ab140a502746a2bbf60a35
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-03-27 11:42:22 +1300

    Fixed up namespace, added code to validate I2C reads & writes

--- SH20DeviceI2C/Program.cs
+++ SH20DeviceI2C/Program.cs
@@ -13,17 +13,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+// nanoff --target ST_STM32F769I_DISCOVERY --update 
+// nanoff --platform ESP32 --serialport COM7 --update
+//
 //---------------------------------------------------------------------------------
-namespace devMobile.IoT.nanoFramework
+//#define ST_STM32F769I_DISCOVERY 
+#define  SPARKFUN_ESP32_THING_PLUS
+namespace devMobile.IoT.Device.Sht20
 {
     using System;
     using System.Device.I2c;
     using System.Threading;
 
+#if SPARKFUN_ESP32_THING_PLUS
+    using nanoFramework.Hardware.Esp32;
+#endif
+
     public class Program
     {
         public static void Main()
         {
+#if SPARKFUN_ESP32_THING_PLUS
+            Configuration.SetPinFunction(Gpio.IO23, DeviceFunction.I2C1_DATA);
+            Configuration.SetPinFunction(Gpio.IO22, DeviceFunction.I2C1_CLOCK);
+#endif
             I2cConnectionSettings i2cConnectionSettings = new(1, 0x40);
 
             // i2cDevice.Dispose
@@ -31,10 +44,21 @@ namespace devMobile.IoT.nanoFramework
 
             while (true)
             {
+                /*
+                byte[] writeBuffer = new byte[3] { 0xF3, 0, 0 };
                 byte[] readBuffer = new byte[3] { 0, 0, 0 };
 
+                i2cDevice.WriteRead(writeBuffer, readBuffer);
+                */
+
+                byte[] readBuffer = new byte[3] { 0, 0, 0 };
+ 
                 // First temperature
-                i2cDevice.WriteByte(0xF3);
+                //i2cDevice.WriteByte(0xF3);
+                if (i2cDevice.WriteByte(0xF3).Status != I2cTransferStatus.FullTransfer)
+                {
+                    Console.WriteLine($"Temperature write failed");
+                }
 
                 //Thread.Sleep(50); // no go -46.8
                 //Thread.Sleep(60);
@@ -42,31 +66,38 @@ namespace devMobile.IoT.nanoFramework
                 //Thread.Sleep(90);
                 //Thread.Sleep(110);
 
-                i2cDevice.Read(readBuffer);
+                // i2cDevice.Read(readBuffer)
+                if ( i2cDevice.Read(readBuffer).Status != I2cTransferStatus.FullTransfer )
+                {
+                    Console.WriteLine($"Temperature read failed");
+                }
 
                 ushort temperatureRaw = (ushort)(readBuffer[0] << 8);
                 temperatureRaw += readBuffer[1];
 
-                //Debug.WriteLine($"Raw {temperatureRaw}");
+                //Console.WriteLine($"Raw {temperatureRaw}");
 
                 double temperature = temperatureRaw * (175.72 / 65536.0) - 46.85;
+
 
                 // Then read the Humidity
                 i2cDevice.WriteByte(0xF5);
 
                 //Thread.Sleep(50);  
                 //Thread.Sleep(60);  
-                Thread.Sleep(70);  
+                Thread.Sleep(70);
                 //Thread.Sleep(90);  
                 //Thread.Sleep(110);   
 
-                
-                i2cDevice.Read(readBuffer);
+                if (i2cDevice.Read(readBuffer).Status != I2cTransferStatus.FullTransfer)
+                {
+                    Console.WriteLine($"Temperature read failed");
+                }
 
                 ushort humidityRaw = (ushort)(readBuffer[0] << 8);
                 humidityRaw += readBuffer[1];
 
-                //Debug.WriteLine($"Raw {humidityRaw}");
+                //Console.WriteLine($"Raw {humidityRaw}");
 
                 double humidity = humidityRaw * (125.0 / 65536.0) - 6.0;
 
